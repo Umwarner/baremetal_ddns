27af8fd86030fe283a728117b8cc74b709d9bdaf
Michael Warner <mike@mwarner.ca>
2023-12-27 16:27:53 -0700

    Initial working copy of code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Simple DDNS utility for baremetal.com domains.
+
+Gets ipv4 from ipify.org, and updates address through 
+http://domain-dns.com/ip.cgi
+
+## USAGE
+
+baremetal_ddns HOST KEY
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+//Simple DDNS utility to update Baremetal.com domains.
+//Gets public ipv4 from ipify.org
+//
+//Usage:
+//baremetal_ddns HOST key
+
+use std::{collections::HashMap, env};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<_> = env::args().collect();
+
+    let response = reqwest::get("https://api.ipify.org?format=json")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+
+    let ip = &response["ip"];
+    let host = &args[1];
+    let key = &args[2];
+    let url = format!(
+        "https://domain-dns.com/ip.cgi?host={}&key={}&ip={}",
+        host, key, ip
+    );
+
+    let response = reqwest::get(url).await?.text().await?;
+    println!("{}", response);
+
+    Ok(())
 }
